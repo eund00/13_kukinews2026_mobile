25514bc197afaa89365059650b3d4d53e64d10a6
eundoo <leeeunj00@naver.com>
2026-03-04 10:34:45 +0900

    feed 이미지 추가 및 마크업 스타일 수정

---
+++ pages/feed.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".gnb_box .swiper", {
+  slidesPerView: "auto", // 자동 너비
+  spaceBetween: 10,
+  freeMode: true, // 자연스러운 스크롤
+});
+
+const swiper2 = new Swiper(".reporter-wrapper .swiper", {
+  slidesPerView: "auto", // 자동 너비
+  spaceBetween: 25,
+  freeMode: true, // 자연스러운 스크롤
+});
